be9ec22aab572812225b4d368cbaa8b159579311
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-01-13 16:41:34 -0500

    commit tree 3339e2ab509f90f85acd4e5fbad2616ebe44584f

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Coupon.create(store: "Chipotle", coupon_code: "ASD123 XYZ098")
